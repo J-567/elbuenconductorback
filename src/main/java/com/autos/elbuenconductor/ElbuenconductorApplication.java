b26f21a24fc5d69b2b20f6a75c38cf3b890ccf92
Fihoca04 <Fihoca04@SINENSIA.EDU>
2019-12-12 08:52:24 +0100

    commit inicial

---
+++ src/main/java/com/autos/elbuenconductor/ElbuenconductorApplication.java
@@ -0,0 +1,13 @@
+package com.autos.elbuenconductor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElbuenconductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElbuenconductorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autos/elbuenconductor/ElbuenconductorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autos.elbuenconductor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElbuenconductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
