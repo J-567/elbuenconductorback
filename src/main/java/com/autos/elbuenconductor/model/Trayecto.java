2da37b1244e43351e87591cc927f6c7ef1229b9b
Tsuzaku <marcsr03@gmail.com>
2019-12-13 12:18:02 +0100

    classTrayecto

---
+++ src/main/java/com/autos/elbuenconductor/model/Trayecto.java
@@ -0,0 +1,13 @@
+package com.autos.elbuenconductor.model;
+
+public class Trayecto {
+
+	private int id;
+	private String matricula;
+	private String kmRecorridos;
+	private int nAcelerones;
+	private int nFrenazos;
+	private double rpmMedias;
+
+	
+}
